04cd90d7d41e4828b202feaffac9733432228f44
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-10-20 10:58:28 -0300

    Criação da classe Mago

---
+++ Mago.java
@@ -0,0 +1,21 @@
+public class Mago {
+    private int pontosDeAtaque = 20;
+    private float pontosDeDefesa = 10;
+    private float pontosDeVida = 200;
+
+    public Mago(){
+
+    }
+
+    public int getPontosDeAtaque(){
+        return this.pontosDeAtaque;
+    }
+
+    public float getPontosDeDefesa(){
+        return this.pontosDeDefesa;
+    }
+
+    public float getPontosDeVida(){
+        return this.pontosDeVida;
+    }
+}
