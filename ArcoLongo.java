4913ef46414b9a35e487f737ce1961ef3f5880ed
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-10-20 11:08:02 -0300

    Criação da classe ArcoLongo

---
+++ ArcoLongo.java
@@ -0,0 +1,16 @@
+public class ArcoLongo {
+    private int adicionalAtaque = 12;
+    private int adicionalDefesa = 13;
+
+    public ArcoLongo(){
+
+    }
+
+    public int getAdicionalAtaque(){
+        return this.adicionalAtaque;
+    }
+
+    public int getAdicionalDefesa(){
+        return this.adicionalDefesa;
+    }
+}
