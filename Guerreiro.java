71b5dfd5c2a6acab1eabbcf10d569b028326a7b2
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-10-20 10:56:06 -0300

    Criação da classe Guerreiro

---
+++ Guerreiro.java
@@ -0,0 +1,21 @@
+public class Guerreiro {
+    private int pontosDeAtaque = 30;
+    private float pontosDeDefesa = 20;
+    private float pontosDeVida = 180;
+
+    public Guerreiro(){
+
+    }
+
+    public int getPontosDeAtaque(){
+        return this.pontosDeAtaque;
+    }
+
+    public float getPontosDeDefesa(){
+        return this.pontosDeDefesa;
+    }
+
+    public float getPontosDeVida(){
+        return this.pontosDeVida;
+    }
+}
