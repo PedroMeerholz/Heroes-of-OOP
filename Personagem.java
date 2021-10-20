0750159762c145692e84d048d7e71bcadc32baa3
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-10-20 10:41:25 -0300

    Criação da classe Personagem

---
+++ Personagem.java
@@ -0,0 +1,15 @@
+public class Personagem {
+    private String nomePersonagem;
+
+    public Personagem(){
+
+    }
+
+    public void setNomePersonagem(String nomePersonagem){
+        this.nomePersonagem = nomePersonagem;
+    }
+
+    public String getNomePersonagem(){
+        return this.nomePersonagem;
+    }
+}
