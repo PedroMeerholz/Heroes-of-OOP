27cb9cb793cbfd8726bda79165e9308b6a8632f5
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-10-20 11:00:32 -0300

    Criação da classe Arqueiro

---
+++ Arqueiro.java
@@ -0,0 +1,21 @@
+public class Arqueiro {
+    private int pontosDeAtaque = 20;
+    private float pontosDeDefesa = 30;
+    private float pontosDeVida = 160;
+
+    public Arqueiro(){
+
+    }
+
+    public int getPontosDeAtaque(){
+        return this.pontosDeAtaque;
+    }
+
+    public float getPontosDeDefesa(){
+        return this.pontosDeDefesa;
+    }
+
+    public float getPontosDeVida(){
+        return this.pontosDeVida;
+    }
+}
