1d1942489df0ed9f45dd447baa33f3593a272ca5
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-10-20 11:01:42 -0300

    Criação da classe Dragao

---
+++ Dragao.java
@@ -0,0 +1,21 @@
+public class Dragao {
+    private int pontosDeAtaque = 30;
+    private float pontosDeDefesa = 30;
+    private float pontosDeVida = 300;
+
+    public Dragao(){
+
+    }
+
+    public int getPontosDeAtaque(){
+        return this.pontosDeAtaque;
+    }
+
+    public float getPontosDeDefesa(){
+        return this.pontosDeDefesa;
+    }
+
+    public float getPontosDeVida(){
+        return this.pontosDeVida;
+    }
+}
