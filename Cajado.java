4317316bd31b72532bb8c3dd09eacb308e2687dd
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-10-20 11:07:10 -0300

    Criação da classe Cajado

---
+++ Cajado.java
@@ -0,0 +1,16 @@
+public class Cajado {
+    private int adicionalAtaque = 13;
+    private int adicionalDefesa = 12;
+
+    public Cajado(){
+
+    }
+
+    public int getAdicionalAtaque(){
+        return this.adicionalAtaque;
+    }
+
+    public int getAdicionalDefesa(){
+        return this.adicionalDefesa;
+    }
+}
