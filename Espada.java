a09e979a8c6e80ebae8a7ff1ba22c9dd192c22af
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-10-20 11:02:59 -0300

    Criação da classe Espada

---
+++ Espada.java
@@ -0,0 +1,16 @@
+public class Espada {
+    private int adicionalAtaque = 10;
+    private int adicionalDefesa = 15;
+
+    public Espada(){
+
+    }
+
+    public int getAdicionalAtaque(){
+        return this.adicionalAtaque;
+    }
+
+    public int getAdicionalDefesa(){
+        return this.adicionalDefesa;
+    }
+}
