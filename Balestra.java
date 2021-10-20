367c98d264ccdb11c367d4cbe711b854c2048819
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-10-20 11:08:54 -0300

    Criação da classe Balestra.java

---
+++ Balestra.java
@@ -0,0 +1,16 @@
+public class Balestra {
+    private int adicionalAtaque = 15;
+    private int adicionalDefesa = 10;
+
+    public Balestra(){
+
+    }
+
+    public int getAdicionalAtaque(){
+        return this.adicionalAtaque;
+    }
+
+    public int getAdicionalDefesa(){
+        return this.adicionalDefesa;
+    }
+}
