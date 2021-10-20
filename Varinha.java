35639195d45c77dce3d088645451d357ac17dae7
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-10-20 11:06:15 -0300

    Criação da classe Varinha

---
+++ Varinha.java
@@ -0,0 +1,16 @@
+public class Varinha {
+    private int adicionalAtaque = 16;
+    private int adicionalDefesa = 9;
+
+    public Varinha(){
+
+    }
+
+    public int getAdicionalAtaque(){
+        return this.adicionalAtaque;
+    }
+
+    public int getAdicionalDefesa(){
+        return this.adicionalDefesa;
+    }
+}
