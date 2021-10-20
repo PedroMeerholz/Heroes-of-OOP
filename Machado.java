29029db847c9456220b0da8f9bf435729f4f4ec4
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-10-20 11:04:29 -0300

    Criação da classe Machado

---
+++ Machado.java
@@ -0,0 +1,16 @@
+public class Machado {
+    private int adicionalAtaque = 17;
+    private int adicionalDefesa = 8;
+    
+    public Machado(){
+
+    }
+
+    public int getAdicionalAtaque(){
+        return this.adicionalAtaque;
+    }
+
+    public int getAdicionalDefesa(){
+        return this.adicionalDefesa;
+    }
+}
