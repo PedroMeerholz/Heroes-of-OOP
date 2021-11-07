f84dab3868e3d2e3c1489d6b6bc3b1fa415e0f73
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-11-06 22:09:04 -0300

    Reescrevendo os métodos da interface AcoesPersonagem na classe Personagem

--- Personagem.java
+++ Personagem.java
@@ -1,4 +1,4 @@
-public class Personagem {
+public class Personagem  implements AcoesPersonagem {
     protected String nomePersonagem;
     protected int pontosDeAtaque;
     protected float pontosDeDefesa;
@@ -48,5 +48,21 @@ public class Personagem {
 
     public String getArmaUtilizada() {
         return armaUtilizada.getNomeArma();
+    }
+
+    @Override
+    public void atacar(Personagem atacante, Personagem alvo) {
+        float dano = atacante.getPontosDeAtaque() - alvo.getPontosDeDefesa();
+
+        float vidaAlvo = alvo.getPontosDeVida() - dano;
+        alvo.setPontosDeVida(vidaAlvo);
+
+        System.out.printf("\nO personagem %s, atacou %s!\nDano Causado: %.1f\nVida atual de %s: %.1f\n\n", atacante.nomePersonagem, alvo.nomePersonagem, dano, alvo.nomePersonagem, alvo.pontosDeVida);
+    }
+
+    @Override
+    public void defender() {
+        this.pontosDeDefesa += (pontosDeDefesa * 0.1);
+        System.out.printf("O personagem %s, obteve um aumento de 10%% em seus pontos de defesa durante esse turno", this.nomePersonagem);
     }
 }
