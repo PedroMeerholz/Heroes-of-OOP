b8fda4260fc751187580b2f4c45b015e4d1f74d2
Pedro Meerholz <meerholz.pedro@gmail.com>
2021-11-06 22:10:23 -0300

    Reescrevendo o método atacar na classe Dragao

--- Dragao.java
+++ Dragao.java
@@ -1,9 +1,22 @@
 public class Dragao extends Personagem {
 
     public Dragao(String nomePersonagem, Arma armaUtilizada) {
-        super("LazyProg", armaUtilizada);
+        super("LazyProg", new Garra());
         setPontosDeAtaque(30);
         setPontosDeDefesa(30);
         setPontosDeVida(300);
+    }
+
+    @Override
+    public void atacar(Personagem atacante, Personagem alvo) {
+        float dano = atacante.getPontosDeAtaque() - alvo.getPontosDeDefesa();
+
+        if(dano > 0) {
+            float vidaAlvo = alvo.getPontosDeVida() - dano;
+            alvo.setPontosDeVida(vidaAlvo);
+            System.out.printf("\n\nO personagem %s, atacou %s!\nDano Causado: %.1f\nVida atual de %s: %.1f\n", atacante.nomePersonagem, alvo.nomePersonagem, dano, alvo.nomePersonagem, alvo.pontosDeVida);
+        } else {
+            System.out.printf("\n\nO personagem %s, não causou nenhum dano", atacante.nomePersonagem);
+        }
     }
 }
